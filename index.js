46390e1afbf7b569d3a8bcd55da63b78f9ba0fba
pgeovany <pgeovany@dcc.ufrj.br>
2022-06-16 18:26:08 -0300

    feat: sign-up post route implemented

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-import express from "express";
+import express, { application, response } from "express";
+import bodyParser from "body-parser";
 import cors from "cors";
 
 const server = express();
 server.use(cors());
+server.use(bodyParser.json());
+
+const users = [];
+
+const tweets = [];
+
+server.post("/sign-up", (request, response) => {
+  //console.log(request.body);
+  //response.send("Ok");
+  users.push({
+    username: request.body.username,
+    avatar: request.body.avatar,
+  });
+  console.log(users);
+  response.send("OK");
+});
 
 server.listen(5000);
