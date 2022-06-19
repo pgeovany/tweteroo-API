a59f2d767890bef3a9ad7d9870fac0aabfbfa4a4
pgeovany <pgeovany@dcc.ufrj.br>
2022-06-19 14:39:41 -0300

    test: validate data on post requests

--- index.js
+++ index.js
@@ -9,25 +9,39 @@ const users = [];
 const tweets = [];
 
 server.post("/sign-up", (request, response) => {
-  users.push({
-    username: request.body.username,
-    avatar: request.body.avatar,
-  });
-  response.send("OK");
+  if (!request.body.username || !isValidURL(request.body.avatar)) {
+    response.status(400).send("Todos os campos são obrigatórios!");
+  } else {
+    users.push({
+      username: request.body.username,
+      avatar: request.body.avatar,
+    });
+    response.status(201).send("OK");
+  }
 });
 
 server.post("/tweets", (request, response) => {
-  const user = users.find((user) => user.username === request.body.username);
-  tweets.unshift({
-    username: user.username,
-    avatar: user.avatar,
-    tweet: request.body.tweet,
-  });
-  response.send("Ok");
+  if (!request.body.username.length || !request.body.tweet.length) {
+    response.status(400).send("Todos os campos são obrigatórios!");
+  } else {
+    const user = users.find((user) => user.username === request.body.username);
+    tweets.unshift({
+      username: user.username,
+      avatar: user.avatar,
+      tweet: request.body.tweet,
+    });
+    response.status(201).send("OK");
+  }
 });
 
 server.get("/tweets", (request, response) => {
   response.send(tweets.slice(-10));
 });
+
+function isValidURL(url) {
+  let regEx =
+    /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm;
+  return regEx.test(url);
+}
 
 server.listen(5000);
